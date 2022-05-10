e238eaec370be738d965c405e36a8bd1045f8605
adriandamiao <79238503+AdrianDamiao@users.noreply.github.com>
2022-05-10 16:11:41 -0300

    Embaralha as imagens e adiciona nos botões

---
+++ app/src/main/java/com/example/jogodamemoria/Element.java
@@ -0,0 +1,22 @@
+package com.example.jogodamemoria;
+
+public class Element {
+    private int image;
+    private String imageText;
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getImageText() {
+        return imageText;
+    }
+
+    public void setImageText(String imageText) {
+        this.imageText = imageText;
+    }
+}
--- app/src/main/java/com/example/jogodamemoria/MainActivity.java
+++ app/src/main/java/com/example/jogodamemoria/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.jogodamemoria;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
 import android.os.Bundle;
 import android.os.Handler;
@@ -8,13 +9,14 @@ import android.view.View;
 import android.widget.Button;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, Runnable{
 
     private Button button1, button2, button3, button4,
                    button5, button6, button7, button8, tempButton;
 
-    private ArrayList<Elemento> elementList;
+    private ArrayList<Element> elementList;
     private Handler handler;
     private int control;
     private String firstClickedButtonText;
@@ -42,8 +44,87 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         button7.setOnClickListener(this);
         button8.setOnClickListener(this);
 
+        tempButton = new Button(this);
+        elementList = new ArrayList<Element>();
+        control = 0;
     }
 
+    public void loadElements(){
+        Element bike1 = new Element();
+        bike1.setImage(R.drawable.ic_action_bike);
+        bike1.setImageText("Bike");
+        elementList.add(bike1);
+
+        Element bike2 = new Element();
+        bike2.setImage(R.drawable.ic_action_bike);
+        bike2.setImageText("Bike");
+        elementList.add(bike2);
+
+        Element boat1 = new Element();
+        boat1.setImage(R.drawable.ic_action_boat);
+        boat1.setImageText("Boat");
+        elementList.add(boat1);
+
+        Element boat2 = new Element();
+        boat2.setImage(R.drawable.ic_action_boat);
+        boat2.setImageText("Boat");
+        elementList.add(boat2);
+
+        Element car1 = new Element();
+        car1.setImage(R.drawable.ic_action_car);
+        car1.setImageText("Car");
+        elementList.add(car1);
+
+        Element car2 = new Element();
+        car2.setImage(R.drawable.ic_action_car);
+        car2.setImageText("Car");
+        elementList.add(car2);
+
+        Element airplane1 = new Element();
+        airplane1.setImage(R.drawable.ic_action_plane);
+        airplane1.setImageText("Airplane");
+        elementList.add(airplane1);
+
+        Element airplane2 = new Element();
+        airplane2.setImage(R.drawable.ic_action_plane);
+        airplane2.setImageText("Airplane");
+        elementList.add(airplane2);
+
+        Collections.shuffle(elementList);
+
+        button1.setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(elementList.get(0).getImage()), null, null);
+        button1.setText(elementList.get(0).getImageText());
+        button1.setEnabled(false);
+
+        button2.setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(elementList.get(1).getImage()), null, null);
+        button2.setText(elementList.get(1).getImageText());
+        button2.setEnabled(false);
+
+        button3.setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(elementList.get(2).getImage()), null, null);
+        button3.setText(elementList.get(2).getImageText());
+        button3.setEnabled(false);
+
+        button4.setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(elementList.get(3).getImage()), null, null);
+        button4.setText(elementList.get(3).getImageText());
+        button4.setEnabled(false);
+
+        button5.setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(elementList.get(4).getImage()), null, null);
+        button5.setText(elementList.get(4).getImageText());
+        button5.setEnabled(false);
+
+        button6.setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(elementList.get(5).getImage()), null, null);
+        button6.setText(elementList.get(5).getImageText());
+        button6.setEnabled(false);
+
+        button7.setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(elementList.get(6).getImage()), null, null);
+        button7.setText(elementList.get(6).getImageText());
+        button7.setEnabled(false);
+
+        button8.setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(elementList.get(7).getImage()), null, null);
+        button8.setText(elementList.get(7).getImageText());
+        button8.setEnabled(false);
+
+    }
     @Override
     public void onClick(View view) {
 
